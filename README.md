662f216df5731c541f71412903644f6779be6496
atomyc <213143875+a-tomy-c@users.noreply.github.com>
2025-07-19 23:21:55 -0400

    agregado widget player-fix: vol, timer

--- README.md
+++ README.md
@@ -5,3 +5,16 @@ video player para generar chapter en un archivo txt
 ## Notas
 
 - agregado y carga de interfaz
+- fix `drag and drop`
+- set volume del player, al usar el slide no cambia el label
+- se arreglo que el timer solo avance cuanod se esta reproduciendo video
+
+
+para arreglar `drag and drop` del widget player anidado:
+
+```python
+self.player.setAcceptDrops(False)
+self.player.video_widget.setAttribute(Qt.WA_TransparentForMouseEvents, True)
+```
+
+en este caso aplique los cambios a la instancia del player
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 from PySide6.QtWidgets import QWidget
+from PySide6.QtCore import Qt
 from skin_mkchapter import Ui_PlayerMakeChapter
+from playerp6 import PlayerP6
 
 
 class MakeChapters(QWidget):
@@ -19,6 +21,28 @@ class MakeChapters(QWidget):
         self.ui.splitter_v.setStretchFactor(0, 1)
         self.ui.tex.setMinimumHeight(110)
         self.ui.lb_capture.setMinimumWidth(180)
+        self.player = PlayerP6()
+        self.ui.vly_player.addWidget(self.player)
+        self.setAcceptDrops(True)
+        self.player.setAcceptDrops(False)
+        self.player.video_widget.setAttribute(Qt.WA_TransparentForMouseEvents, True)
+        self.player.timer.timeout.connect(self.update_time_label)
+
+    def dragEnterEvent(self, event):
+        if event.mimeData().hasUrls():
+            event.acceptProposedAction()
+        
+    def dropEvent(self, event):
+        urls = event.mimeData().urls()
+        if urls:
+            filepath = urls[0].toLocalFile()
+            print(filepath)
+            self.player.set_videopath(filepath)
+        event.acceptProposedAction()
+
+    def update_time_label(self):
+        res= self.player.get_current_time()
+        self.ui.lb_time.setText(res)
 
 
 if __name__=="__main__":
